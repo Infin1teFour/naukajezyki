448feba19481b360be54a4bf3745f871dd590950
Babo2137 <babooo121201@gmail.com>
2023-10-24 09:00:40 +0200

    edit

--- easy.py
+++ easy.py
@@ -1,5 +1,20 @@
-import tkinter as tk
+import tkinter as tk  # biblioteka tkinter  jako tk
+from tkinter import messagebox  # messagebox z biblioteki tkinter
+from PIL import ImageTk, Image  #  Image i ImageTk z biblioteki PIL (pillow)
+import mysql.connector  # łącznik mysql
+import random as r  # biblioteka random jako r
+import matplotlib.pyplot as plt  # biblioteka matplotlib jako plt
 
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="trudnosci"
+)
+
+
+
+#okno 
 root = tk.Tk()
 root.title("nauka - poziom łatwy")
 root.geometry("1200x800")
@@ -12,24 +27,38 @@ def test():
 
 
 
-easy = tk.Button(root, text="1", width=20, height=2, command= test)
-easy.grid(row=4, column=0)
 
-easy = tk.Button(root, text="2", width=20, height=2, command= test)
-easy.grid(row=4, column=1)
+#zmienne
+a = 0
+b = 0
+c = 0
+d = 0
+e = 0
+f = 0
 
-easy = tk.Button(root, text="3", width=20, height=2, command= test)
-easy.grid(row=4, column=2)
 
-easy = tk.Button(root, text="4", width=20, height=2, command= test)
-easy.grid(row=5, column=0)
 
-easy = tk.Button(root, text="5", width=20, height=2, command= test)
-easy.grid(row=5, column=1)
 
-easy = tk.Button(root, text="6", width=20, height=2, command= test)
-easy.grid(row=5, column=2)
 
+
+#przyciski
+a1 = tk.Button(root, text="1", width=20, height=2, command= test)
+a1.grid(row=4, column=0)
+
+b1 = tk.Button(root, text="2", width=20, height=2, command= test)
+b1.grid(row=4, column=1)
+
+c1 = tk.Button(root, text="3", width=20, height=2, command= test)
+c1.grid(row=4, column=2)
+
+d1 = tk.Button(root, text="4", width=20, height=2, command= test)
+d1.grid(row=5, column=0)
+
+e1 = tk.Button(root, text="5", width=20, height=2, command= test)
+e1.grid(row=5, column=1)
+
+f1 = tk.Button(root, text="6", width=20, height=2, command= test)
+f1.grid(row=5, column=2)
 
 
 
