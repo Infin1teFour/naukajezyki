d5cff9ad8133c5a0fee37dd4c9552c9ee2bc3912
Yla07 <bastianwic07@wp.pl>
2023-10-25 16:14:21 +0200

    Dobry start wpoprawie kodu

--- hard.py
+++ hard.py
@@ -5,44 +5,46 @@ root = tk.Tk()
 root.title("Nauka - Trudne")
 root.resizable(0, 0)
 
-wordlist = ["apple", "banana", "pickle"]
-wordlist = random.sample(wordlist, 3)
+wordlist = [("jabłko", "apple"), ("banan", "banana"), ("ogórek", "pickle")]
+random.shuffle(wordlist)
 
-display = wordlist
 score = 0
+current_polish, current_english = wordlist[score]
 
-currentword = tk.Label(root, text=display, font=("Arial", 20), width=25, height=2)
-currentword.grid(row=0, column=0, columnspan=3)
+def pytanie():
+    return current_polish
+
+pytanie_L = tk.Label(root, text="", font=("Arial", 20), width=25, height=2)
+pytanie_L.grid(row=0, column=0, columnspan=3)
+
+currentword = tk.Label(root, text=current_polish, font=("Arial", 20), width=25, height=2)
+currentword.grid(row=1, column=0, columnspan=3)
 
 scoredis = tk.Label(root, text="Punkty: " + str(score), font=("Arial", 20), width=25, height=2)
 scoredis.grid(row=0, column=3)
 
 guess = tk.StringVar()
 guess_entry = tk.Entry(root, width=20, font=("Arial", 20), textvariable=guess)
-guess_entry.grid(row=1, column=0)
+guess_entry.grid(row=2, column=0)
 
 def check():
-    global display, score, wordlist
+    global score, current_polish, current_english, wordlist
     if score < len(wordlist):
-        correct = wordlist[score]
-        display = ""
-        if guess.get() == correct:
+        if guess.get() == current_english:
             score += 1
             scoredis.config(text="Punkty: " + str(score))
             if score < len(wordlist):
-                display = "_ " * len(wordlist[score])
-            currentword.config(text=display)
+                current_polish, current_english = wordlist[score]
+                currentword.config(text=current_polish)
+                pytanie_L.config(text="")
+                guess.set("")
+            else:
+                currentword.config(text="Gra zakończona")
         else:
-            display = ""
-            for i in range(len(correct)):
-                if i < len(guess.get()) and guess.get()[i] == correct[i]:
-                    display += guess.get()[i]
-                else:
-                    display += "_ "
-            currentword.config(text=display)
+            pytanie_L.config(text="Zła odpowiedź")
 
 button = tk.Button(root, text="Sprawdź", width=20, height=2, command=check)
-button.grid(row=1, column=1)
+button.grid(row=2, column=1)
 
 check()
 tk.mainloop()
