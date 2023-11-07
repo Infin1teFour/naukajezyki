3e5b9a3551a153dc468c93b6f3ce7cd154f99d58
Yla07 <bastianwic07@wp.pl>
2023-11-07 08:21:25 +0100

    commit

--- medhard.py
+++ medhard.py
@@ -1,12 +1,38 @@
 import tkinter as tk
+import mysql.connector
+import random
 
+# Połączenie z bazą danych MySQL
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="trudnosci"
+)
+
+# Inicjalizacja głównego okna Tkinter
 root = tk.Tk()
-root.title("nauka - poziom Średnnio-Trudny")
-root.geometry("1200x800")
-root.resizable("false", "false")
+root.title("Nauka - Trudne")
+root.resizable(0, 0)
 
+# Inicjalizacja zmiennych
+score = 0
+current_finish =  ""
 
-print("Poziom medhard")
+# Inicjalizacja zmiennej do śledzenia liczby błędnych odpowiedzi
+loser = 0
 
+# Funkcja do pobierania słówek z bazy danych
+def get_wordlist_from_database():
+    cursor = db.cursor()
+    cursor.execute("SELECT napis_polski, fin_slowo FROM poziom3")
+    return cursor.fetchall()
 
-root.mainloop()
+# Funkcja do losowania nowego słówka
+def choose_random_word():
+    global  current_finish
+    current_finish = random.choice(wordlist)
+
+# Przygotowanie listy słówek
+wordlist = get_wordlist_from_database()
+choose_random_word()
